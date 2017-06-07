7338c32548967ff1a46afb45c78252ef1615df06
MissakBoyajian <missakboya@live.com>
2017-06-07 20:25:18 +0300

    Last Seen

--- FootballChallenge/www/js/homepagecontroller.js
+++ FootballChallenge/www/js/homepagecontroller.js
@@ -230,7 +230,7 @@ angular.module('football.controllers')
                             }
                             else {
                                 var alertPopup = $ionicPopup.alert({
-                                   template: 'Sorry, the team does not exist anymore.'
+                                    template: 'Sorry, the team does not exist anymore.'
                                 }).then(function () {
                                     HomeStore.DeleteInvitation(invitation).then(function () {
                                         $scope.profile.teaminvitations = $scope.profile.teaminvitations.filter(function (el) {
@@ -392,6 +392,36 @@ angular.module('football.controllers')
             picture: "",
             rank: ""
         }
+
+
+
+        // since I can connect from multiple devices or browser tabs, we store each connection instance separately
+        // any time that connectionsRef's value is null (i.e. has no children) I am offline
+        var myConnectionsRef = firebase.database().ref('lastonlines/rudKv3IU2oaQ10Xe9E4aQ5zXIzz2/connections');
+
+        // stores the timestamp of my last disconnect (the last time I was seen online)
+        var lastOnlineRef = firebase.database().ref('lastonlines/rudKv3IU2oaQ10Xe9E4aQ5zXIzz2/connections/last');
+
+        var connectedRef = firebase.database().ref('.info/connected');
+
+        connectedRef.on('value', function (snap) {
+            if (snap.val() === true) {
+                // We're connected (or reconnected)! Do anything here that should happen only if online (or on reconnect)
+
+                // add this device to my connections list
+                // this value could contain info about the device or a timestamp too
+                var con = myConnectionsRef.push(true);
+
+                // when I disconnect, remove this device
+                con.onDisconnect().remove();
+
+                // when I disconnect, update the last time I was seen online
+                lastOnlineRef.onDisconnect().set(firebase.database.ServerValue.TIMESTAMP);
+            }
+        });
+
+
+
 
 
 
